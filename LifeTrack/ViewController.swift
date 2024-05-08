b7e4dd2713bc02a285dc7acc7023185b067d1658
egor-galkin29 <egorgalkin290907@gmail.com>
2024-05-08 12:48:22 -0400

    Initial Commit

---
+++ LifeTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeTrack
+//
+//  Created by Егор Галкин on 2024-05-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
